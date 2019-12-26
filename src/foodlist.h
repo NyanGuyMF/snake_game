e0024d972321c78020f1beee942d36b53a138e86
Vasiliy <vasyokbelyivnew@gmail.com>
2019-12-26 21:36:31 +0500

    Add newline characters after function defenitions

--- src/foodlist.h
+++ src/foodlist.h
@@ -18,9 +18,11 @@ typedef struct {
 } foodlist_t;
 
 foodlist_t *fl_new(uint8_t max);
+
 void fl_free(foodlist_t *list);
 
 bool fl_add(foodlist_t *fl, struct point coords);
+
 void fl_rem(foodlist_t *fl, uint8_t index);
 
 void fl_clear(foodlist_t *fl, WINDOW *window);
